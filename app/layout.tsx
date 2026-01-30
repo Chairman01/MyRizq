7f2530d2e871cd7e174877e74f90128aaa3d404b
Abdul Abdihalim <abdulabdihalim@gmail.com>
2026-01-29 23:14:19 -0700

    Update favicon and layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ export const metadata: Metadata = {
   description: 'Discover, compare, and learn about Shariah-compliant Stocks, ETFs, Sukuks, and more. Your comprehensive resource for halal investing.',
   keywords: 'Halal ETF, Shariah compliant investing, Islamic finance, SPUS, HLAL, Muslim investing, ethical ETF, halal stocks',
   icons: {
-    icon: '/custom-favicon.png',
-    apple: '/custom-favicon.png',
+    icon: '/Favicon/favicon-32x32.png',
+    apple: '/Favicon/apple-touch-icon.png',
   },
   generator: 'Next.js',
   alternates: {
